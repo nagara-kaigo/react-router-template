852f5e614fa0fc795962f674e9c99d14b9710877
yuta0709 <58354812+yuta0709@users.noreply.github.com>
2025-04-18 01:44:03 +0900

    Setup deploy workflow

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,15 @@
 import type { Config } from "@react-router/dev/config";
+import { copyFile } from "node:fs/promises";
+import path from "node:path";
 
 export default {
   // Config options...
+  basename: import.meta.env.PROD ? "/react-router-template" : "/",
+  async buildEnd(args) {
+    if (!args.viteConfig.isProduction) return;
+    const out = args.viteConfig.build.outDir;
+    await copyFile(path.join(out, "index.html"), path.join(out, "404.html"));
+  },
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
 } satisfies Config;
